aad71af37647153ecb7970cab4faa69c1a48e92a
gonzaOlarte <50810570+gonzaOlarte@users.noreply.github.com>
2022-10-27 18:03:26 -0300

    Vistas
    Acá vamos a ir agregando las vistas:
    1. V_Compras_UltimasCompras

---
+++ SqlQueries/Vistas.sql
@@ -0,0 +1,23 @@
+USE [Grupo42]
+GO
+
+/************** CREACIÓN DE VISTAS **************/
+
+-- CREA V_Compras_UltimasCompras
+/****** V_Compras_UltimasCompras    Script Date: 27/10/2022 17:58:12 ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE VIEW [dbo].[V_Compras_UltimasCompras]
+AS
+SELECT        TOP (100) PERCENT dbo.Compra.idCompra, dbo.Compra.Total, CONVERT(varchar, dbo.Compra.FechaCompra, 103) AS Expr1, dbo.Compra.TipoComprobante, dbo.Proveedor.RazonSocial, dbo.Producto.Cantidad, 
+                         dbo.Producto.Nombre
+FROM            dbo.Compra INNER JOIN
+                         dbo.DetalleCompra ON dbo.Compra.idCompra = dbo.DetalleCompra.idCompra INNER JOIN
+                         dbo.Producto ON dbo.DetalleCompra.idProducto = dbo.Producto.idProducto INNER JOIN
+                         dbo.Proveedor ON dbo.Producto.idProveedor = dbo.Proveedor.idProveedor
+ORDER BY dbo.Compra.FechaCompra DESC
+GO
